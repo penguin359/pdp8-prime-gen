d0bc67b12f29df80e8204e65dafa2a149e665650
Loren M. Lang <lorenl@north-winds.org>
2014-05-05 19:02:19 -0700

    First attempt at supported 6-bit ASCII printing, doesn't work
    Add symbol table to listing file, generate a RIM output file, and print
    out error messages on failure.

--- primes.sh
+++ primes.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-macro8x primes.pal
+macro8x -d primes.pal
 ret=$?
 if [ $ret -ne 0 ]; then
-	echo "ERROR" >&2
+	#echo "ERROR" >&2
+	cat primes.err >&2
 	exit 1
 fi
 pdp8 primes.cmd
+macro8x -d -r primes.pal
