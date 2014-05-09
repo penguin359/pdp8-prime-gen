5e1e20e6ea820cc4743c05efcf403555ed4f9586
Loren M. Lang <lorenl@north-winds.org>
2014-05-08 21:01:22 -0700

    Made test a little more verbose

--- primes-test.sh
+++ primes-test.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-./primes.sh | tr -d '\r' | awk 'BEGIN {P=0} /^END$/ {P=0} P==1 {print} /^BEGIN$/ {P=1}' | diff - primes-ref
+#./primes.sh | tr -d '\r' | awk 'BEGIN {P=0} /^END$/ {P=0} P==1 {print} /^BEGIN$/ {P=1}' | diff - primes-ref
+./primes.sh | tr -d '\r' | awk 'BEGIN {P=0} /^END$/ {P=0} P==1 {print} /^BEGIN$/ {P=1}' > pdp8-out
+cat pdp8-out
+if diff pdp8-out primes-ref; then
+	echo "Test passed."
+else
+	echo "ERROR: Output did not match reference."
+	exit 1
+fi
