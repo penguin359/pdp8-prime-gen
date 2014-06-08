7a3d8aafb21f362d730cfb0f585da5427462f2b7
Loren M. Lang <lorenl@north-winds.org>
2014-06-07 23:40:58 -0700

    Drop the mark parity in hi/hello programs
    Fixed executable bit on scripts

---
+++ hello.cmd
@@ -0,0 +1,3 @@
+load hello.bin
+run
+quit
--- hello.sh
+++ hello.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 cd $(dirname $0)
-macro8x -d hi.pal
+macro8x -d hello.pal
 ret=$?
 if [ $ret -ne 0 ]; then
 	#echo "ERROR" >&2
-	cat hi.err >&2
+	cat hello.err >&2
 	exit 1
 fi
-pdp8 hi.cmd
-macro8x -d -r hi.pal
+pdp8 hello.cmd
+macro8x -d -r hello.pal
